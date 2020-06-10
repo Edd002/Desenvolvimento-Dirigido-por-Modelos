ddb2b4fc7117c19280867c3a3c038bd4ff071c13
Edd002 <42621512+Edd002@users.noreply.github.com>
2020-06-10 13:35:59 -0300

    Initial commit

---
+++ Última Prova/Avaliação final_Parte 5.sql
@@ -0,0 +1,15 @@
+INSERT INTO Administrador VALUES (1,'Administrador 1','admin1@email.com','Login1','Senha1'),(2,'Administrador 2','admin2@email.com','Login 2','Senha 2'),(3,'Administrador 3','admin3@email.com','Login3','Senha3'),(4,'Administrador 4','admin4@email.com','Login4','Senha4'),(5,'Administrador 5','admin5@email.com','Login 5','Senha 5');
+
+INSERT INTO Associado VALUES (1,'Cliente 1','863.543.538-91','Endereço 11','11111-1111','cliente1@email.com',500,1),(2,'Cliente 2','438.053.863-03','Endereço 22','22222-2222','associado2@email.com',300,2),(3,'Cliente 3','296.040.883-78','Endereço 33','33333-3333','cliente3@email.com',100,3),(4,'Cliente 4','756.041.536-99','Endereço 44','44444-4444','cliente4@email.com',100,4),(5,'Cliente 5','324.576.345-04','Endereço 55','55555-5555','cliente5@email.com',50,5),(6,'Cliente 6','248.057.016-92','Endereço 66','66666-6666','cliente6@email.com',25,6);
+
+INSERT INTO Beneficio VALUES (1,'Benefício 1','01/01/2020',100),(2,'Benefício 2','02/02/2020',200),(3,'Benefício 3','03/03/2020',300),(4,'Benefício 4','04/04/2020',400),(5,'Benefício 5','05/05/2020',500),(6,'Benefício 6','06/06/2020',600);
+
+INSERT INTO Dependente VALUES (1,'Dependente 1','844.946.426-90','Endereço 11','11111-1111','dependente1@email.com',0,1),(2,'Dependente 2','327.730.538-42','Endereço 11','11111-1111','dependente2@email.com',0,1),(3,'Dependente 3','736.685.325-91','Endereço 11','11111-1111','dependente3@email.com',0,3),(4,'Dependente 4','867.515.547-61','Endereço 2','22222-2222','dependente2@email.com',0,2),(5,'Dependente 5','755.883.061-39','Endereço 5','55555-5555','dependente5@email.com',0,5);
+
+INSERT INTO Parceiro VALUES (1,'Parceiro 1','46.475.333/0001-78','Endereço 1','1111-1111','parceiro1@email.com'),(2,'Parceiro 2','21.396.234/0001-70','Endereço 2','2222-2222','parceiro2@email.com'),(3,'Parceiro 3','53.448.244/0001-06','Endereço 3','3333-3333','parceiro3@email.com'),(4,'Parceiro 4','31.866.025/0001-04','Endereço 4','4444-4444','parceiro4@email.com'),(5,'Parceiro 5','82.692.633/0001-72','Endereço 5','5555-5555','parceiro5@email.com');
+
+INSERT INTO Plano VALUES (1,'Ouro','Semestral',500),(2,'Prata','Semestral',300),(3,'Bronze','Semestral',100),(4,'Ouro','Mensal',100),(5,'Prata','Mensal',50),(6,'Bronze','Mensal',25);
+
+INSERT INTO Plano_Beneficio VALUES (1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5),(6,6,6);
+
+INSERT INTO Produto VALUES (1,'Produto 1',50,1,1),(2,'Produto 2',100,2,2),(3,'Produto 3',150,3,3),(4,'Produto 4',200,4,4),(5,'Produto 5',250,5,5);
